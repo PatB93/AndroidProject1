ae70e3e24428152271b2427d613003a80ce3a2c6
PatB93 <pat.b.irish@gmail.com>
2018-06-13 21:21:21 -0400

    WIP

---
+++ app/src/main/java/service/MainServiceImpl.kt
@@ -0,0 +1,9 @@
+package service
+
+import io.reactivex.Observable
+
+class MainServiceImpl {
+    fun getEquipmentList() : Observable<List<EquipmentResponse>> {
+        return getEquipmentList()
+    }
+}
--- app/src/main/java/userintent/MainIntent.kt
+++ app/src/main/java/userintent/MainIntent.kt
@@ -4,14 +4,23 @@ import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
 import com.example.pbode.dndshopgenerator.ShopActivity
+import service.EquipmentResponse
+import service.MainServiceImpl
 import state.MainState
 import viewevent.ViewEvent
 
 class MainIntent : LifecycleObserver {
 
+    val serviceRequest : MainServiceImpl = MainServiceImpl()
+
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
         MainState.title.set("This is the title")
+        serviceRequest.getEquipmentList().subscribe({
+            for (item in it) {
+                print(item.itemName)
+            }
+        })
     }
 
     fun onBackPressed() {
